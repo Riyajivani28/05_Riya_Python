d23c004a777c67e7da4c4a413f3790b8ceccc9e4
riyajivani85 <riyajivani85@gmail.com>
2026-07-23 10:47:53 +0530

    session-5

---
+++ Assignment/Module-4/session-5/adminfilter/myapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Restaurant
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'cuisine', 'rating']
+    search_fields = ['name', 'cuisine']
+    list_filter = ['cuisine']
+
+
+admin.site.register(Restaurant, RestaurantAdmin)
